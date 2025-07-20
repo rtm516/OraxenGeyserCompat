803a3beeab78f4ee6b19f4c9acd248755de2a712
rtm516 <ryantmilner@hotmail.co.uk>
2025-07-20 16:46:49 +0100

    Fix block rendering in world

--- src/main/java/com/rtm516/oraxengeysercompat/OraxenGeyserCompat.java
+++ src/main/java/com/rtm516/oraxengeysercompat/OraxenGeyserCompat.java
@@ -6,13 +6,13 @@ import io.th0rgal.oraxen.api.OraxenPack;
 import io.th0rgal.oraxen.api.events.OraxenPackGeneratedEvent;
 import io.th0rgal.oraxen.items.ItemBuilder;
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.geysermc.geyser.api.GeyserApi;
 import org.geysermc.geyser.api.block.custom.CustomBlockData;
+import org.geysermc.geyser.api.block.custom.CustomBlockState;
 import org.geysermc.geyser.api.block.custom.component.BoxComponent;
 import org.geysermc.geyser.api.block.custom.component.CustomBlockComponents;
 import org.geysermc.geyser.api.block.custom.component.GeometryComponent;
@@ -21,8 +21,6 @@ import org.geysermc.geyser.api.event.EventRegistrar;
 import org.geysermc.geyser.api.event.bedrock.SessionLoadResourcePacksEvent;
 import org.geysermc.geyser.api.event.lifecycle.GeyserDefineCustomBlocksEvent;
 import org.geysermc.geyser.api.event.lifecycle.GeyserDefineCustomItemsEvent;
-import org.geysermc.geyser.api.item.custom.CustomItemData;
-import org.geysermc.geyser.api.item.custom.CustomItemOptions;
 import org.geysermc.geyser.api.item.custom.v2.CustomItemBedrockOptions;
 import org.geysermc.geyser.api.item.custom.v2.CustomItemDefinition;
 import org.geysermc.geyser.api.pack.PackCodec;
@@ -32,10 +30,7 @@ import org.geysermc.geyser.api.util.Identifier;
 import org.geysermc.pack.converter.PackConverter;
 import org.geysermc.pack.converter.converter.Converter;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.List;
 import java.util.ServiceLoader;
 
@@ -122,34 +117,15 @@ public final class OraxenGeyserCompat extends JavaPlugin implements EventRegistr
                 .selectionBox(box)
                 .build();
 
-            CustomBlockData.Builder data = CustomBlockData.builder()
+            CustomBlockData data = CustomBlockData.builder()
                 .name(entry.getKey())
-                .components(components);
+                .components(components)
+                .build();
 
-            // minecraft:note_block[instrument=basedrum,note=2,powered=false]
-            String blockDataString = blockData.getAsString();
-            String[] properties = blockDataString.substring(blockDataString.indexOf("[") + 1, blockDataString.indexOf("]")).split(",");
+            CustomBlockState.Builder stateBuilder = data.blockStateBuilder();
 
-            for (String pair : properties) {
-                String[] keyValue = pair.split("=");
-                String key = keyValue[0];
-                String value = keyValue[1];
-
-                // if value boolean
-                if (value.equals("true") || value.equals("false")) {
-                    if (value.equals("true")) data.booleanProperty(key);
-                } else if (value.matches("\\d+")) {
-                    // if value int
-                    data.intProperty(key, Collections.singletonList(Integer.parseInt(value)));
-                } else {
-                    // if value string
-                    data.stringProperty(key, Collections.singletonList(value));
-                }
-            }
-
-            CustomBlockData finalData = data.build();
-            event.register(finalData);
-            event.registerItemOverride(blockData.getAsString(), finalData);
+            event.register(data);
+            event.registerOverride(blockData.getAsString(), stateBuilder.build());
         });
     }
 
